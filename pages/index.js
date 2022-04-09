ff28c95c43b5aadaf6505fcfe90d344599515271
Nikita Brancatisano <theniki98@gmail.com>
2022-04-09 18:31:15 +0200

    fix: Add Temp Dialogue to Index

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,90 @@ import Image from "next/image";
 import ChatArea from "../components/ChatArea";
 import styles from "../styles/Home.module.css";
 
+const dialogue = {
+  id: 0,
+  messages: [
+    {
+      id: 0,
+      messages: [
+        "Why would Artificial Intelligence kill us all?",
+        "We're a bunch of nerds who have thought about the future of AI a lot and have concluded that future AI systems are quite likely to destroy humanity.",
+        "We know that sounds weird. You probably disagree. Click on why you disagree and see if we know something you don't.",
+      ],
+      choices: [
+        {
+          text: "Artificial Intelligence as smart as humans is impossible because…",
+          next: 1,
+        },
+        {
+          text: "If an AI does bad things we just turn it off",
+          next: 2,
+        },
+        {
+          text: " Smart people are nice, smart machines will also be nice",
+          next: 3,
+        },
+      ],
+    },
+    {
+      id: 1,
+      messages: [
+        "You've chosen 1",
+        "What do you think computers can't do that we can",
+      ],
+      choices: [
+        {
+          text: "Computers can't innovate",
+          next: 1,
+        },
+        {
+          text: "Computers can't be creative",
+          next: 2,
+        },
+        {
+          text: " Smart people are nice, smart machines will also be nice",
+          next: 3,
+        },
+      ],
+    },
+    {
+      id: 2,
+      messages: [
+        "You've chosen number 2",
+        "How do you think that should work?",
+      ],
+      choices: [
+        {
+          text: "Scenario 1",
+          next: 1,
+        },
+        {
+          text: "Scenario 2",
+          next: 2,
+        },
+        {
+          text: "Scenario 3",
+          next: 3,
+        },
+      ],
+    },
+    {
+      id: 3,
+      messages: ["You've chosen 3", "Who do you think will take care of it?"],
+      choices: [
+        {
+          text: "Government",
+          next: 1,
+        },
+        {
+          text: "Tech companies",
+          next: 2,
+        },
+      ],
+    },
+  ],
+};
+
 export default function Home() {
   return (
     <div style={{ backgroundColor: "#282a36" }} className={styles.container}>
@@ -13,7 +97,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <ChatArea />
+        <ChatArea conversation={dialogue} />
       </main>
     </div>
   );
